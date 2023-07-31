1d61f36462c0f0d8ba2196c9ad7c829fbf04ef28
Todd Barrett <toddcbarrett3@gmail.com>
2023-07-31 09:55:59 -0700

    Completed Intro Exercise

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Testing.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}

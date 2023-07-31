016b9b7548bc5b73d386dd88fb2f1f58f875ecc0
Todd Barrett <toddcbarrett3@gmail.com>
2023-07-31 11:44:12 -0700

    Completed Exercise 2

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -16,5 +16,10 @@ namespace Testing.Models
         {
             return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
         }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
     }
 }

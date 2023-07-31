cb5840c5fa64e07ec7ab36054fd27a1ea6091910
Todd Barrett <toddcbarrett3@gmail.com>
2023-07-31 11:56:19 -0700

    Completed Exercise 3

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -11,18 +11,31 @@ namespace Testing.Controllers
         {
             this.repo = repo;
         }
-
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
             
             return View(products);
         }
-
         public IActionResult ViewProduct(int id)
         {
             var product = repo.GetProduct(id);
             return View(product);
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
     }
 }
--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -5,6 +5,10 @@ namespace Testing.Models
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+        
         public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+
     }
 }
